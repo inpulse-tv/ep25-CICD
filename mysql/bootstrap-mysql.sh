c031c1c29741b912a82e723e4cd4679853d31fb5
grondinl <lo.grondin@gmail.com>
2022-04-08 17:15:06 +0200

    Update mysql bootstrap file #4

--- mysql/bootstrap-mysql.sh
+++ mysql/bootstrap-mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mysql -h 127.0.0.1 -e "CREATE DATABASE pouletmayo"
-mysql -h 127.0.0.1 --default-character-set=utf8 pouletmayo < dump.sql
+mysql -u root -h 127.0.0.1 -e "CREATE DATABASE pouletmayo"
+mysql -u root -h 127.0.0.1 --default-character-set=utf8 pouletmayo < dump.sql
