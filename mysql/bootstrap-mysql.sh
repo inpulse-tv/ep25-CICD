4c7792cd4d0385e41614a270875ab82a101a68ed
grondinl <lo.grondin@gmail.com>
2022-04-08 12:52:37 +0200

    Add bootstrap to docker compose

--- mysql/bootstrap-mysql.sh
+++ mysql/bootstrap-mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker cp dump.sql pouletmayo-db-1:dump.sql
-docker exec -it --env MYSQL_PWD=${MYSQL_ROOT_PASSWORD} pouletmayo-db-1 bash -c "mysql -u root  -e \"CREATE DATABASE pouletmayo\""
-docker exec -it --env MYSQL_PWD=${MYSQL_ROOT_PASSWORD} pouletmayo-db-1 bash -c "mysql -u root  --default-character-set=utf8 pouletmayo < dump.sql"
+set -x
+mysql -u root --password=${MYSQL_PASSWORD} -h db -e "CREATE DATABASE pouletmayo"
+mysql -u root --password=${MYSQL_PASSWORD} -h db --default-character-set=utf8 pouletmayo < /mysql-script/dump.sql
