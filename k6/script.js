bccd9e80ccdfd572155453e88dec7f9e38b5d783
grondinl <lo.grondin@gmail.com>
2022-04-12 15:25:11 +0200

    Update to workflow to push to githuv registry

--- k6/script.js
+++ k6/script.js
@@ -2,7 +2,7 @@ import { describe } from 'https://jslib.k6.io/expect/0.0.4/index.js';
 import http from 'k6/http';
 
 export default function () {
-  describe('Fetch today\'s entres', (t) => {
+  describe('Fetch today\'s entrees', (t) => {
     const response = http.get('http://localhost:8000/api/entreesdujour');
 
     t.expect(response.status)
