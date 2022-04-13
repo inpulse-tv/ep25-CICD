1946dbee1b134b0fa4753741656de859d2ca7713
grondinl <lo.grondin@gmail.com>
2022-04-13 14:51:02 +0200

    Update k6 script to fail when error

--- k6/script.js
+++ k6/script.js
@@ -1,6 +1,12 @@
 import { describe } from 'https://jslib.k6.io/expect/0.0.4/index.js';
 import http from 'k6/http';
 
+export const options = {
+  thresholds: {
+    checks: [{threshold: 'rate == 1.00',abortOnFail: true }],
+  }
+};
+
 export default function () {
   describe('Fetch today\'s entrees', (t) => {
     const response = http.get('http://localhost:8000/api/entreesdujour');
